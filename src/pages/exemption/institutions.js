20cd1608c195234ca0195709f6a53d0125b66e11
shahi7 <azshahid2013@gmail.com>
2026-09-17 19:45:59 -0400

    email domains

--- src/pages/exemption/institutions.js
+++ src/pages/exemption/institutions.js
@@ -22,25 +22,209 @@ export const ALLOWED_INSTITUTIONS = [
 ];
 
 export const INSTITUTIONAL_EMAIL_DOMAINS = [
-  "algomau.ca",
-  "brocku.ca",
-  "carleton.ca",
-  "lakeheadu.ca",
-  "laurentian.ca",
-  "mcmaster.ca",
-  "nipissingu.ca",
-  "ocadu.ca",
-  "ontariotechu.ca",
-  "queensu.ca",
-  "torontomu.ca",
-  "trentu.ca",
-  "uoguelph.ca",
-  "uottawa.ca",
-  "utoronto.ca",
-  "uwaterloo.ca",
-  "uwindsor.ca",
-  "uwo.ca",
-  "westernu.ca",
-  "wlu.ca",
-  "yorku.ca"
+  	
+	"acadiau.ca",
+		
+	"assumptionu.ca",
+		
+	"athabascau.ca",
+		
+	"bcit.ca",
+		
+	"bcou.ca",
+		
+	"boothcollege.ca",
+		
+	"bowvalleycollege.ca",
+		
+	"brandonu.ca",
+		
+	"brocku.ca",
+		
+	"capilanou.ca",
+		
+	"carleton.ca",
+		
+	"cbu.ca",
+		
+	"ccbc.ca",
+		
+	"cegepdrummond.ca",
+		
+	"cegepmontpetit.ca",
+		
+	"centennialcollege.ca",
+		
+	"citecollegiale.ca",
+		
+	"columbiacollege.ca",
+		
+	"concordia.ca",
+		
+	"cus.ca",
+		
+	"cuslm.ca",
+		
+	"dal.ca",
+		
+	"devry.ca",
+		
+	"ecuad.ca",
+		
+	"etsmtl.ca",
+		
+	"fanshaweonline.ca",
+		
+	"firstnationsuniversity.ca",
+		
+	"hec.ca",
+		
+	"humbermail.ca",
+		
+	"inrs.ca",
+		
+	"lakeheadu.ca",
+		
+	"laurentian.ca",
+		
+	"mcgill.ca",
+		
+	"mcmaster.ca",
+		
+	"mohawkcollege.ca",
+		
+	"msvu.ca",
+		
+	"mta.ca",
+		
+	"mun.ca",
+		
+	"myseneca.ca",
+		
+	"mytru.ca",
+		
+	"nait.ca",
+		
+	"nbcc.ca",
+		
+	"nipissingu.ca",
+		
+	"nscc.ca",
+		
+	"ocad.ca",
+		
+	"polymtl.ca",
+		
+	"queensu.ca",
+		
+	"questu.ca",
+		
+	"redeemer.ca",
+		
+	"rmc.ca",
+		
+	"royalroads.ca",
+		
+	"rrc.ca",
+		
+	"ryerson.ca",
+		
+	"sait.ca",
+		
+	"saskpolytech.ca",
+		
+	"saultcollege.ca",
+		
+	"sfu.ca",
+		
+	"stfx.ca",
+		
+	"stmarys.ca",
+		
+	"stthomasu.ca",
+		
+	"stu.ca",
+		
+	"trentu.ca",
+		
+	"tru.ca",
+		
+	"twu.ca",
+		
+	"ualberta.ca",
+		
+	"ubc.ca",
+		
+	"ubishops.ca",
+		
+	"ucalgary.ca",
+		
+	"ufv.ca",
+		
+	"ulaval.ca",
+		
+	"uleth.ca",
+		
+	"umanitoba.ca",
+		
+	"umoncton.ca",
+		
+	"umontreal.ca",
+		
+	"unb.ca",
+		
+	"unbc.ca",
+		
+	"unbsj.ca",
+		
+	"universitycanadawest.ca",
+		
+	"uoguelph.ca",
+		
+	"uoit.ca",
+		
+	"uottawa.ca",
+		
+	"upei.ca",
+		
+	"uqac.ca",
+		
+	"uqam.ca",
+		
+	"uqo.ca",
+		
+	"uqtr.ca",
+		
+	"uquebec.ca",
+		
+	"uregina.ca",
+		
+	"usask.ca",
+		
+	"usherb.ca",
+		
+	"usherbrooke.ca",
+		
+	"ustpaul.ca",
+		
+	"utoronto.ca",
+		
+	"uvic.ca",
+		
+	"uwaterloo.ca",
+		
+	"uwindsor.ca",
+		
+	"uwinnipeg.ca",
+		
+	"uwo.ca",
+		
+	"vcc.ca",
+		
+	"viu.ca",
+		
+	"wlu.ca",
+		
+	"yorku.ca",
+		
 ];
